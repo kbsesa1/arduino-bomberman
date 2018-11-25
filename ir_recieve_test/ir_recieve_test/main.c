2f5e8854e20942be475860f3342a96c05444ad65
Joost Hartkamp <joosthartkamp@hotmail.com>
2018-11-25 19:15:55 +0100

    ir library with test projects

---
+++ ir_recieve_test/ir_recieve_test/main.c
@@ -0,0 +1,18 @@
+/*
+ * ir_recieve_test.c
+ *
+ * Created: 25-11-2018 19:13:59
+ * Author : joost
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
